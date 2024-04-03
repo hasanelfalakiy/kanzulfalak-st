c6a51c058fd744bc6c7c2df9b4abbccd3596e351
andihasan97 <andihasanelfalakiy@gmail.com>
2024-04-03 08:47:26 +0700

    add: license

--- function.py
+++ function.py
@@ -1,4 +1,22 @@
-def toCounter2(decimal):
+"""
+ * This file is part of kanzulfalak-st.
+ *
+ * kanzulfalak-st is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * kanzulfalak-st is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with kanzulfalak-st.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ """
+ 
+ def toCounter2(decimal):
     time = abs(int(decimal))
     minute = int((abs(decimal) - time) * 60)
     second = round((((abs(decimal) - time) * 60) - minute) * 60, 2)
